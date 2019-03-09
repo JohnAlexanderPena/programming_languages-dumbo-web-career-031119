608f8169215ccd27bee292fe7e4654fb67333eed
John Pena <jpena91@gmail.com>
2019-03-09 19:12:44 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,10 +6,10 @@ def reformat_languages(languages)
     language_hash.each do |language, attribute_hash|
       attribute_hash.each do |attribute, string|
         if hash[language].nil?
-          binding.pry
           hash[language] = {}
         end 
         hash[language][:style] ||= []
+        binding.pry
         hash[language][:style] << oo_functional
         if hash[language][attribute].nil?
           hash[language][attribute] = string
